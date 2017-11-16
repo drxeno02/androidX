7a306abe10d8668447e020c6cac9919691d8d9a4
Leonard Tatum <ljtatum@hotmail.com>
2017-11-16 00:08:15 -0800

    made application operation on Android 8.0 operating system

--- app/src/main/java/com/blog/ljtatum/androidx/gui/iPhoneNotch.java
+++ app/src/main/java/com/blog/ljtatum/androidx/gui/iPhoneNotch.java
@@ -2,6 +2,7 @@ package com.blog.ljtatum.androidx.gui;
 
 import android.content.Context;
 import android.graphics.PixelFormat;
+import android.os.Build;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -34,18 +35,34 @@ public class iPhoneNotch extends View {
      * Method is used to add iPhoneX notch to window manager
      */
     private void addNotchToWindowManager() {
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                // Allows the view to be on top of the StatusBar
-                WindowManager.LayoutParams.TYPE_SYSTEM_ERROR,
-                // Keeps the button presses from going to the background window
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
-                        // Enables the notification to recieve touch events
-                        WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
-                        // Draws over status bar
-                        WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
-                PixelFormat.TRANSLUCENT);
+        WindowManager.LayoutParams params;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            params = new WindowManager.LayoutParams(
+                    WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.WRAP_CONTENT,
+                    // Allows the view to be on top of the StatusBar
+                    WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                    // Keeps the button presses from going to the background window
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
+                            // Enables the notification to recieve touch events
+                            WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+                            // Draws over status bar
+                            WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
+                    PixelFormat.TRANSLUCENT);
+        } else {
+            params = new WindowManager.LayoutParams(
+                    WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.WRAP_CONTENT,
+                    // Allows the view to be on top of the StatusBar
+                    WindowManager.LayoutParams.TYPE_SYSTEM_ERROR,
+                    // Keeps the button presses from going to the background window
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
+                            // Enables the notification to recieve touch events
+                            WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+                            // Draws over status bar
+                            WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
+                    PixelFormat.TRANSLUCENT);
+        }
         params.gravity = Gravity.TOP | Gravity.CENTER_HORIZONTAL;
 
         // return the handle to a system-level service by name
