aa35761299bfc87de4236ce6ee372ad86581fe40
drxeno02 <ljtatum@hotmail.com>
2019-12-29 14:21:41 -0800

    update gitignore

---
+++ app/build/generated/source/buildConfig/standard/release/com/blog/ljtatum/androidx/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.blog.ljtatum.androidx;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.blog.ljtatum.androidx";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "standard";
+  public static final int VERSION_CODE = 6;
+  public static final String VERSION_NAME = "1.0.5";
+  // Fields from build type: release
+  public static final boolean DEBUG_MODE = false;
+  public static final String ENVIRONMENT = "production";
+}
